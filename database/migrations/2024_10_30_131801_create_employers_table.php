f293a083dfec49e5722170a674c4f528d17ec0b6
gyamficodes <gyamfijohn08@gmail.com>
2024-10-30 14:49:43 +0000

    working on database.

--- database/migrations/2024_10_30_131801_create_employers_table.php
+++ database/migrations/2024_10_30_131801_create_employers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\User;
 return new class extends Migration
 {
     /**
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create("employers",  function (Blueprint $table) {
-              $table->id();
-              $table->string("name");
-              $table->timestamps();
+        Schema::create('employers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->string('logo');
+            $table->timestamps();
         });
     }
 
@@ -23,7 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
-        
+        Schema::dropIfExists('employers');
     }
 };
