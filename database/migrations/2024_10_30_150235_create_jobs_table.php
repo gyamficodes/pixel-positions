686b668aaf5eedcba9aa539533c8390219b49cde
gyamficodes <gyamfijohn08@gmail.com>
2024-11-01 01:30:20 +0000

    Working on  Database Relation

--- database/migrations/2024_10_30_150235_create_jobs_table.php
+++ database/migrations/2024_10_30_150235_create_jobs_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use \App\Models\Employer;
 return new class extends Migration
 {
     /**
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Employer::class);
+            $table->string('title');
+            $table->string('salary');
+            $table->string('location');
+            $table->string('type')->default('Full Time');
+            $table->string('url');
+            $table->boolean('featured')->default(false);
             $table->timestamps();
         });
     }
