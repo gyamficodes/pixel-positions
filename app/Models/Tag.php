aa05194ac2ef08bb7144b9abec7434cb02faf609
gyamficodes <gyamfijohn08@gmail.com>
2024-11-19 13:12:03 +0000

    update

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
@@ -12,4 +12,9 @@ class Tag extends Model
     
     protected $table = 'tags';
     protected  $fillable = ['name'];
+
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class);
+    }
 }
--- resources/views/components/page-heading.blade.php
+++ resources/views/components/page-heading.blade.php
@@ -1,1 +1,3 @@
-<h1 class=" font-bold text-4xl  mb-8">{{ $slot }}</h1>
+<h1 class=" font-bold text-4xl  mb-8 text-center">{{ $slot }}</h1>
+
+
